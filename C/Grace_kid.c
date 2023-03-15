7a952f96a14dd309933682a133c2b1bfc9ccd62d
Jeremy <jeremy.perras@outlook.fr>
2023-03-15 14:50:04 +0100

    Collen

--- C/Grace_kid.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define GRACE_CODE "#include <stdio.h>%c#define GRACE_CODE %c%s%c%c#define GRACE() int main(){FILE *Grace= fopen(%cGrace_kid.c%c, %cw%c);if(Grace==NULL) return(1);fprintf(Grace,GRACE_CODE, 10, 34,GRACE_CODE, 34, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10); RETURN;}%c#define RETURN return 0%cGRACE()%c/*%cThis program will print its own source when run.%c*/"
-#define GRACE() int main(){FILE *Grace= fopen("Grace_kid.c", "w");if(Grace==NULL) return(1);fprintf(Grace,GRACE_CODE, 10, 34,GRACE_CODE, 34, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10); RETURN;}
-#define RETURN return 0
-GRACE()
-/*
-This program will print its own source when run.
-*/
