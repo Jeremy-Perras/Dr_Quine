ae46a687223895e11c4f4fa40edd41ade741020b
Jeremy Perras <jeremy.perras@outlook.fr>
2023-03-22 09:12:31 +0100

    Fix path

--- PYTHON/Sully.py
+++ PYTHON/Sully.py
@@ -2,6 +2,8 @@ import os
 i = 5
 if i > 0:
     with open("Sully_"+str(i-1)+".py", "w") as file2:
-        s = 'import os\ni = {0}\nif i > 0:\n    with open("Sully_"+str(i-1)+".py", "w") as file2:\n        s = {1}\n        file2.write(s.format(i-1, repr(s)))\n    os.system("/Users/jperras/goinfre/.brew/bin/python3 /Users/jperras/Desktop/Dr_Quine-42/PYTHON/Sully_"+str(i-1)+".py")\n'
+        directory = os.getcwd()
+        print(directory)
+        s = 'import os\ni = {0}\nif i > 0:\n    with open("Sully_"+str(i-1)+".py", "w") as file2:\n        directory = os.getcwd()\n        print(directory)\n        s = {1}\n        file2.write(s.format(i-1, repr(s)))\n    os.system("/Users/jperras/goinfre/.brew/bin/python3 "+directory+"/Sully_"+str(i-1)+".py")\n'
         file2.write(s.format(i-1, repr(s)))
-    os.system("/Users/jperras/goinfre/.brew/bin/python3 /Users/jperras/Desktop/Dr_Quine-42/PYTHON/Sully_"+str(i-1)+".py")
+    os.system("/Users/jperras/goinfre/.brew/bin/python3 "+directory+"/Sully_"+str(i-1)+".py")
